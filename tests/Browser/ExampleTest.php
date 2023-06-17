92c4519daa2640012a8e42cb19d48ba13caf608f
hugoresende27 <hugoresende27@gmail.com>
2023-06-17 10:16:34 +0100

    tests : browser

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Browser;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
@@ -21,13 +22,48 @@ class ExampleTest extends DuskTestCase
     /** @test  */
     public function check_if_login_function_is_working()
     {
-        $this->browse(function (Browser $browser) {
+
+        $user = User::factory()->create([
+            'email' => 'taylor'.rand(1,99).'@laravel.com',
+        ]);
+
+        $this->browse(function (Browser $browser) use ($user) {
             $browser->visit('/login')
-                ->type('email', 'hugo@hugo')
-                ->type('password', '12345678')
+                ->type('email', $user->email)
+                ->type('password', 'password')
                 ->press('Login')
                 ->assertPathIs('/home');
         });
 
     }
+
+
+
+    /** @test  */
+    public function check_if_register_function_is_working()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/register')
+                ->type('name', 'hugo22') // Modify the CSS selector to target the name input field correctly
+                ->type('email', 'hugo55@hugo')
+                ->type('password', '12345678')
+                ->type('password_confirmation', '12345678')
+                ->press('Register')
+                ->assertPathIs('/home')
+                ->assertSee('Dashboard');
+
+        });
+
+
+    }
+
+    /** @test  */
+    public function check_if_dashboard_function_is_working()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/home')
+                ->assertSee('You are logged in!');
+        });
+    }
+
 }
